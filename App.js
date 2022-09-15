2d5668236f74b4ff2a2f88506451f4b59c72cdf0
triaditiyanto <ferarilima1234@gmail.com>
2022-09-15 16:01:05 +0700

    Updates

--- App.js
+++ App.js
@@ -16,11 +16,11 @@ class TodoApp extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-        <h3>ReactOdo</h3>
+        <h3>TODO</h3>
         <TodoList items={this.state.items} />
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="new-todo">
-            Please note your schedule 
+            Please note your schedule !
           </label>
           <input
             id="new-todo"
